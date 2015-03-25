0e6b59fa3172d8eef4dc82e31dd23c9304837881
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-03-25 13:45:49 +0000

    Rewrote varfun to have RenameVariables, true(def)/false instead

--- +tbextend/varfun.m
+++ +tbextend/varfun.m
@@ -4,13 +4,13 @@ function b = varfun(fun,a,varargin)
 % 
 %   Since TABLE/VARFUN automatically pre-pends the variable names
 %   with the function name, this wrapper additionally accepts the 
-%   option 'VariableNames'.
+%   name/value pair: 'RenameVariables', true(default)/false.
 %   
 % Example:
 %   
 %   myVar = rand(10,1);
 %   tb    = table(myVar);
-%   tb    = varfun(@uint32, tb, 'VariableNames','UintVar')
+%   tb    = varfun(@uint32, tb, 'RenameVariables', false)
 %
 % See also: TABLE
 
@@ -18,30 +18,47 @@ function b = varfun(fun,a,varargin)
 % Tested on R2014b Win7 64bit
 % 24 Mar 2015 - Created
 
-% Extract variable names option
-pos = find(strcmpi('VariableNames', varargin));
-if ~isempty(pos)
-    outputNames = varargin{pos+1};
+% Parse name/value pairs
+pnames = {'GroupingVariables' 'InputVariables' 'OutputFormat' 'RenameVariables'};
+dflts =  {                []               ':'        'table'             true };
+[groupVars,dataVars,outputFormat,renameVars,supplied] ...
+    = matlab.internal.table.parseArgs(pnames, dflts, varargin{:});
+
+% Extarct RenameVariables from varargin
+if supplied.RenameVariables
+    pos = find(strcmpi('RenameVariables', varargin));
     varargin(pos:pos+1) = [];
 end
 
 % Call builtin method
 b = varfun(fun, a, varargin{:});
 
-% If output not a table
-pos = find(strcmpi('OutputFormat', varargin));
-if ~isempty(pos) && ~strcmpi(varargin{pos+1}, 'table')
+% Terminate if output not a table
+if supplied.OutputFormat && ~strcmpi(outputFormat, 'table')
+    return
+end
+% Terminate if we prepend Func_* by default
+if ~supplied.RenameVariables || renameVars
     return
 end
 
-% Extract eventual input variables
-inputVars = ':';
-pos       = find(strcmpi('InputVariables', varargin));
-if ~isempty(pos)
-    inputVars = varargin{pos+1};
-    if isstring(inputVars) || iscellstr(inputVars)
-        [~, inputVars] = ismember(inputVars, a.Properties.VariableNames);
+% Variable names to keep
+if supplied.InputVariables
+    inputVars = ':';
+    if isnumeric(dataVars) || islogical(dataVars)
+        outputNames = a.Properties.VariableNames(dataVars);
+    else
+        outputNames = dataVars;
     end
+end
+if supplied.GroupingVariables
+    if isnumeric(groupVars) || islogical(groupVars)
+        groupVars   = [a.Properties.VariableNames(groupVars), 'GroupCount'];
+    else
+        groupVars   = [groupVars, {'GroupCount'}];
+    end
+    inputVars   = ~ismember(b.Properties.VariableNames, groupVars);
+    outputNames = outputNames(~ismember(outputNames, groupVars));
 end
 
 % Rename variables
