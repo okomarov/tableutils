37668cfb5ff854f1e6ec90a6c8296f25d2b9958c
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-06-15 22:04:44 +0100

    Aloow vector bounds

--- in.m
+++ in.m
@@ -1,20 +1,22 @@
 function idx = in(A, bounds, inclusion)
 % idx = in(A, bounds, inclusion)
 
-bounds = sort(bounds);
+bounds = sort(bounds,2);
 if nargin < 3 || isempty(inclusion)
     inclusion = '[]';
 end
 
+lb = bounds(:,1);
+ub = bounds(:,2);
 switch inclusion
     case '[]'
-        idx = bounds(1) <= A & A <= bounds(2);
+        idx = lb <= A & A <= ub;
     case '()'
-        idx = bounds(1) <  A & A <  bounds(2);
+        idx = lb <  A & A <  ub;
     case '[)'
-        idx = bounds(1) <= A & A <  bounds(2);
+        idx = lb <= A & A <  ub;
     case '(]'
-        idx = bounds(1) <  A & A <= bounds(2);
+        idx = lb <  A & A <= ub;
     otherwise
         error
 end
