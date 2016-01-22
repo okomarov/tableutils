6f999bb062e16f605e8331c13c251a92098dea5a
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-01-22 20:00:46 +0000

    add ismatrixlike method

---
+++ methods/ismatrixlike.m
@@ -0,0 +1,15 @@
+function bool = ismatrixlike(t)
+% ISMATRIXLIKE True if one value per table cell
+% 
+% NOTE: it tests the condition size(table) == size(table{:,:}) without
+% actually extracting and concatenating the data.
+bool = true;
+sz = size(t);
+for ii = 1:sz(2)
+    [r,c] = size(t{:,ii});
+    if c > 1
+        bool = false;
+        return
+    end
+end
+end
