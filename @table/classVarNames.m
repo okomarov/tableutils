fcc26f3ab65cc953d9c3d15a32c2d55df325869a
oleg komarov <o.komarov11@imperial.ac.uk>
2016-01-25 02:23:53 +0000

    classVarNames include fallback indexing

--- @table/classVarNames.m
+++ @table/classVarNames.m
@@ -1,11 +1,14 @@
 function out = classVarNames(tb)
 % CLASSVARNAMES Return class name of each column of a table
-
-vnames = getProperty(tb,'VariableNames');
+vnames = tb.Properties.VariableNames;
 n      = numel(vnames);
 c      = cell(1,n);
 for ii = 1:n
-    c{ii} = class(tb.data{ii});
+    try
+        c{ii} = class(tb.data{ii});
+    catch
+        c{ii} = class(tb{1,ii});
+    end
 end
 if nargout == 0
     disp(cell2table(c,'VariableNames',vnames))
